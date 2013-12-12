1010745a3e9f7ea7cd7cc958dafef5b73c314e92
Lauren Zou <zou.lauren@gmail.com>
2013-12-12 16:23:03 -0500

    Added easy challenge #3

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ My solutions to Daily Programmer.
 ## Challenges (Easy)
 * [**Challenge #1**](http://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/) - MATLAB
 * [**Challenge #2**](http://www.reddit.com/r/dailyprogrammer/comments/pjbj8/easy_challenge_2/) - MATLAB
+* [**Challenge #3**](http://www.reddit.com/r/dailyprogrammer/comments/pkw2m/2112012_challenge_3_easy/) - Java
 * [**Challenge #34**](http://www.reddit.com/r/dailyprogrammer/comments/rmmn8/3312012_challenge_34_easy/) - MATLAB
 * [**Challenge #144: Nuts & Bolts**](http://www.reddit.com/r/dailyprogrammer/comments/1sob1e/121113_challenge_144_easy_nuts_bolts/) - PHP
---
+++ challenge easy 003/CaesarCipher.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class CaesarCipher {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        // Get key
+        System.out.print("Key = ");
+        int key = in.nextInt();
+        in.nextLine();
+
+        // Get message
+        System.out.print("Message = ");
+        String message = in.nextLine();
+        String code = encrypt(key, message);
+
+        System.out.println("\nEncoded with key = " + key + "\n" + code);
+        System.out.println("\nDecoded with key = " + key + "\n" + decrypt(key, code));
+    }
+
+    public static String encrypt(int key, String message) {
+        char[] messageArray = message.toCharArray();
+        for (int i = 0; i < messageArray.length; i++) {
+            messageArray[i] = shift(key, messageArray[i]);
+        }
+        return new String(messageArray);
+    }
+
+    public static String decrypt(int key, String message) {
+        char[] messageArray = message.toCharArray();
+        for (int i = 0; i < messageArray.length; i++) {
+            messageArray[i] = shift(-key, messageArray[i]);
+        }
+        return new String(messageArray);
+    }
+
+    private static char shift(int key, char c) {
+        if (c >= 'A' && c <= 'Z') {
+            char shifted = (char)((c - 'A' + key)%('Z'-'A'+1) + 'A');
+            while (shifted < 'A')
+                shifted += 'Z'-'A'+1;
+            return shifted;
+        }
+        if (c >= 'a' && c <= 'z') {
+            char shifted = (char)((c - 'a' + key)%('z'-'a'+1) + 'a');
+            while (shifted < 'a')
+                shifted += 'z'-'a'+1;
+            return shifted;
+        }
+        return c;
+    }
+}
