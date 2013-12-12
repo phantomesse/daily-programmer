7bd572eb1df53ddaff49c6c200d62105708b58c9
Lauren Zou <zou.lauren@gmail.com>
2013-12-12 14:25:14 -0500

    First commit. Added easy challenges #1 and #144.

---
+++ challenge easy 001/nameageusername.m
@@ -0,0 +1,5 @@
+name = input('What is your name? ', 's');
+age = input('How old are you? ');
+username = input('What is your username? ', 's');
+
+disp(['your name is ', name, ', you are ', num2str(age), ' years old, and your username is ', username]);
---
+++ challenge easy 144/nutsandbolts.php
@@ -0,0 +1,63 @@
+<?php
+
+function nuts_and_bolts($filename) {
+    $filereader = fopen($filename,'r');
+    
+    # Get number of rows
+    $rows = fgets($filereader);
+    
+    # Fill out the old list
+    $old_list = array();
+    for ($i = 0; $i < $rows; $i++) {
+        $line = split(" ", fgets($filereader));
+        $old_list[$line[0]] = $line[1];
+    }
+
+    # Scan through new list, filling out changes array
+    $changes = array();
+    for ($i = 0; $i < $rows; $i++) {
+        $line = split(" ", fgets($filereader));
+        $change = $line[1] - $old_list[$line[0]];
+        if ($change !== 0) {
+            $changes[$line[0]] = $change;
+        }
+    }
+
+    # Print changes
+    while($change = current($changes)) {
+        $key = key($changes);
+        echo "$key ";
+        if ($changes[$key] > 0) {
+            echo "+".$changes[$key]."<br />";
+        } else {
+            echo $changes[$key]."<br />";
+        }
+        next($changes);
+    }
+    
+    fclose($filereader);
+}
+
+function print_input($filename) {
+    $filereader = fopen($filename, 'r');
+
+    while (($line = fgets($filereader)) !== false) {
+        echo $line."<br />";
+    }
+
+    fclose($filereader);
+}
+
+echo "<h1>INPUT</h1>";
+print_input("nutsandbolts_input1.txt");
+echo "<h1>OUTPUT</h1>";
+nuts_and_bolts("nutsandbolts_input1.txt");
+
+echo "<hr>";
+
+echo "<h1>INPUT</h1>";
+print_input("nutsandbolts_input2.txt");
+echo "<h1>OUTPUT</h1>";
+nuts_and_bolts("nutsandbolts_input2.txt");
+
+?>
